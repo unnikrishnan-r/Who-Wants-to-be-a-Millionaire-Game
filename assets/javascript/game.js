26c5ac871bfe7989d74393dff24f92ccf33c094d
Unni Ramachandran <unnipbvr@gmail.com>
2019-06-16 17:05:51 -0400

    Basic HTML, Rules of the games and js file with pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+/*********************************************************************************
+**********************VARIABLE DECLARATIONS HERE********************************** 
+*********************************************************************************/
+// Array with master list of simple words - 100
+// Array with master list of medium words - 30
+// Array with master list of complex words - 30
+
+
+/*********************************************************************************
+**********************OBJECT DECLARATIONS HERE************************************ 
+*********************************************************************************/
+// Array with objects as members, where computer has choosen 5 words randomly from
+// the master list
+
+/*********************************************************************************
+**********************FUNCTIONS DEFINED HERE************************************** 
+*********************************************************************************/
+//makeComputerMakeTheChoice()
+//Choose 3 random numbers - from simple words array get 3 words
+//Choose 1 random number - from medium words array get 1 word
+//Choose 1 random number - from complex words array get 1 word
+//Populate question object with the question number, question and price money
+
+
+//askQuestion ()
+// Initialize variables Qustion, Current Word, Guesses So far, Guesses Left
+// Accept Question Number as Argument
+// Access object and obtain question, answer and price money
+// Throw information on the screen
+
+//validateUserGuess()
+//Populate user key to array of guessed so far
+//If user guessed key is in currentword, then reveal and populate userChoice array
+//If user guessed key is not in current word, decrement guess Count
+//If userChoice array = currentWord, then user wins
+// Increment price money & Display press any key for next question
+
+/*********************************************************************************
+***************************MAIN PROCESS HERE************************************** 
+*********************************************************************************/
+
+
+// CAPTURE THE KEY STROKE EVENT
+
+//CHECK IF THE GAME HAS STARTED
+
+//IF THE GAME HAS NOT STARTED
+//   INITIALIZE THE COUNTERS - WINS, PRICE MONEY, Q
+//   Call makeComputerMakeTheChoice()
+
+
+// IF THE GAME HAS STARTED
+//  CHECK IF GUESS COUNTER = 0, IF NO
+//  Call validateUserGuess()     
---
+++ rulesofthegame.md
@@ -0,0 +1,13 @@
+# Rules for Who wants to be a Millionaire
+
+## Levels of the Game and scores
+
+    1. One game consists of 5 questions
+    2. The hardness levels increases by question and so does the rewards
+       Q1 - $25K , Q2 - $75K, Q3 - $150K Q4 - $250K Q5 - $500K
+    2. Computer will randomly choose a word which the player has to guess
+    3. Player gets 2 times the length of the word as chances to guess the correct answer
+       eg: if the answer is Texas, player can use up to 10 key strokes to correctly guess the word
+    4. If a player correctly typed a letter that is part of the answer, the corresponding position will revealed on the screen
+    5. If the number of guesses run out, game moves to the next question
+
